81993837fb34b310a0e6a9d750fda33532bbc69e
Filippo Ceruti <rodsdealmeida@gmail.com>
2023-11-20 09:34:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+AOS.init();
+
+let show = true;
+const menuContent = document.querySelector('.header-content');
+const menuToggle = menuContent.querySelector('.menu-toggle');
+
+
+menuToggle.addEventListener('click', () => {
+
+    document.body.style.overflow = show ? 'hidden' : 'initial'
+
+    menuContent.classList.toggle('on', show);
+    show = !show;
+})
